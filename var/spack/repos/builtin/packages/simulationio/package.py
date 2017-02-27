5254f0ebfc87dea58a1f04d13fec6c49829812a4
Erik Schnetter <schnetter@gmail.com>
2017-02-27 10:32:31 -0500

    simulationio: Work on using cmake for building

--- var/spack/repos/builtin/packages/simulationio/package.py
+++ var/spack/repos/builtin/packages/simulationio/package.py
@@ -1,61 +1,45 @@
 from spack import *
 import glob
 import os
-import shutil
-import sys
 
-class Simulationio(Package):
+class Simulationio(CMakePackage):
     """SimulationIO: Efficient and convenient I/O for large PDE simulations"""
     homepage = "https://github.com/eschnett/SimulationIO"
+    url= "https://github.com/eschnett/SimulationIO/archive/version/0.1.0.tar.gz"
 
+    # version('0.1.0', '00f7dabc08ed1ab77858785ce0809f50')
     version('master',
             git='https://github.com/eschnett/SimulationIO.git', branch='master')
+    version('cmake',
+            git='https://github.com/eschnett/SimulationIO.git',
+            branch='eschnett/cmake')
 
     variant('julia', default=False)
     variant('python', default=True)
 
-    depends_on('curl')
-    depends_on('swig')
+    variant('pic', default=True,
+            description="Produce position-independent code")
 
-    depends_on('hdf5')
-    depends_on('mpi')
+    depends_on('julia', when='+julia', type=('build', 'run'))
+    depends_on('py-h5py', when='+python', type=('build', 'run'))
+    depends_on('py-numpy', when='+python', type=('build', 'run'))
+    depends_on('python@2.7.0:2.999.999', when='+python', type=('build', 'run'))
+    depends_on('swig', type='build')
 
-    depends_on('julia', when='+julia')
-    depends_on('python', when='+python')
-    # depends_on('py-h5py', when='+python')
-    # depends_on('py-numpy', when='+python')
+    def cmake_args(self):
+        spec = self.spec
+        os.environ["PYTHONPATH"] = ":".join(
+            [
+                glob.glob(spec['py-h5py'].prefix + "/lib/python2.7/site-packages/h5py-*")[0],
+                glob.glob(spec['py-numpy'].prefix + "/lib/python2.7/site-packages/numpy-*")[0],
+                os.environ.get("PYTHONPATH", "")
+            ])
+        options = []
+        if '+pic' in spec:
+            options.extend(["-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"])
+        return options
 
-    def install(self, spec, prefix):
-        # Build
-        make('CXX=%s' % spec['mpi'].mpicxx,
-             'HDF5_DIR=%s' % spec['hdf5'].prefix,
-             'MPI_DIR=%s' % spec['mpi'].prefix,
-             'PYTHON_DIR=%s' % spec['python'].prefix)
-
-        # # Test
-        # make('check')
-
-        # Install
-        shutil.rmtree(join_path(prefix, 'bin'), ignore_errors=True)
-        shutil.rmtree(join_path(prefix, 'include'), ignore_errors=True)
-        shutil.rmtree(join_path(prefix, 'lib'), ignore_errors=True)
-        os.mkdir(join_path(prefix, 'bin'))
-        os.mkdir(join_path(prefix, 'include'))
-        os.mkdir(join_path(prefix, 'lib'))
-        for binfile in ['test_RegionCalculus', 'test_SimulationIO',
-                        'sio-benchmark',
-                        'sio-copy', 'sio-example', 'sio-list', 'sio-merge',
-                        'sio-convert-carpet-output']:
-            shutil.copy(binfile, join_path(prefix, 'bin'))
-        for incfile in glob.iglob('*.hpp'):
-            shutil.copy(incfile, join_path(prefix, 'include'))
-        for libfile in glob.iglob('*.a'):
-            shutil.copy(libfile, join_path(prefix, 'lib'))
-        for libfile in glob.iglob('*.dylib'):
-            shutil.copy(libfile, join_path(prefix, 'lib'))
-        for libfile in glob.iglob('*.so'):
-            shutil.copy(libfile, join_path(prefix, 'lib'))
-        for pyfile in ['H5.py', 'RegionCalculus.py', 'SimulationIO.py']:
-            shutil.copy(pyfile, join_path(prefix, 'lib'))
-        if sys.platform == 'darwin':
-            fix_darwin_install_name(prefix.lib)
+    def check(self):
+        with working_dir(self.build_directory):
+            # self._if_make_target_execute('test')
+            make("test", "CTEST_OUTPUT_ON_FAILURE=1")
