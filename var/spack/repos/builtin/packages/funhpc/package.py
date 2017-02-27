43fbacba06d9db2604da076630e8d0eca344a998
Erik Schnetter <schnetter@gmail.com>
2017-02-27 10:32:13 -0500

    funhpc: Correct package urls

--- var/spack/repos/builtin/packages/funhpc/package.py
+++ var/spack/repos/builtin/packages/funhpc/package.py
@@ -2,15 +2,16 @@ from spack import *
 
 class Funhpc(CMakePackage):
     """FunHPC: Functional HPC Programming"""
-    homepage = "https://bitbucket.org/eschnett/funhpc.cxx"
+    homepage = "https://github.com/eschnett/FunHPC.cxx"
     url= "https://github.com/eschnett/FunHPC.cxx/archive/version/0.1.0.tar.gz"
 
+    version('1.1.1', '7b9ef638b02fffe35b75517e8eeff580')
     version('1.1.0', '897bd968c42cd4f14f86fcf67da70444')
     version('1.0.0', 'f34e71ccd5548b42672e692c913ba5ee')
     version('0.1.1', 'f0248710f2de88ed2a595ad40d99997c')
     version('0.1.0', '00f7dabc08ed1ab77858785ce0809f50')
     version('master',
-            git='https://bitbucket.org/eschnett/funhpc.cxx', branch='master')
+            git='https://github.com/eschnett/FunHPC.cxx', branch='master')
 
     variant('pic', default=True,
             description="Produce position-independent code")
@@ -27,3 +28,5 @@ class Funhpc(CMakePackage):
         if '+pic' in spec:
             options.extend(["-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"])
         return options
+
+    # TODO: run tests with "make test CTEST_OUTPUT_ON_FAILURE=1"
