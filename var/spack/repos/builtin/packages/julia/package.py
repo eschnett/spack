5d142d08bb147f2d39f1446b6ebf8fc60f8b41e5
Erik Schnetter <schnetter@gmail.com>
2017-03-15 22:07:52 -0400

    julia: Add simd variant

--- var/spack/repos/builtin/packages/julia/package.py
+++ var/spack/repos/builtin/packages/julia/package.py
@@ -55,6 +55,7 @@ class Julia(Package):
             description="Install Julia plotting packages")
     variant("python", default=False,
             description="Install Julia Python package")
+    variant("simd", default=False, description="Install Julia SIMD package")
 
     patch('gc.patch', when='@0.4:0.4.5')
     patch('openblas.patch', when='@0.4:0.4.5')
@@ -234,5 +235,9 @@ using UnicodePlots
 unicodeplots()
 plot(x->sin(x)*cos(x), linspace(0, 2pi))
 """)
+
+        # Install SIMD
+        if "+simd" in spec:
+            julia("-e", 'Pkg.add("SIMD"); using SIMD')
 
         julia("-e", 'Pkg.status()')
