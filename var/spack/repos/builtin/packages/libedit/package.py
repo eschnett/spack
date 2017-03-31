c5cfc8b272f4dfd10dda84564c388cf61ccbd9dd
Erik Schnetter <schnetter@gmail.com>
2017-03-31 12:35:12 -0400

    libedit: Correct version numbering scheme; update to version 0.56.0

--- var/spack/repos/builtin/packages/libedit/package.py
+++ var/spack/repos/builtin/packages/libedit/package.py
@@ -30,7 +30,18 @@ class Libedit(AutotoolsPackage):
     homepage = "http://thrysoee.dk/editline/"
     url      = "http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz"
 
-    version('3.1', '43cdb5df3061d78b5e9d59109871b4f6',
+    # Note: The "3.1" in the file name is NOT the version of this
+    # library, but rather (probably) the shared library compatibility
+    # version number, which is irrelevant for Spack. (That is, you
+    # would expect a file named "lib/libedit.so.3.1" or
+    # "lib/libedit.3.1.dylib" to be installed.)
+
+    # The real version numbers are listed only in the change log,
+    # using the format "0:56:0".
+
+    version('0.56.0', 'c57a0690e62ef523c083598730272cfd',
+            url="http://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz")
+    version('0.53.0', '43cdb5df3061d78b5e9d59109871b4f6',
             url="http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz")
 
     depends_on('ncurses')
