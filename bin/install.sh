544bbf08f540465346fb4a0e9bedb97ff3fd4f8b
Erik Schnetter <schnetter@gmail.com>
2018-01-04 12:58:52 -0500

    Update compiler versions

--- bin/install.sh
+++ bin/install.sh
@@ -5,22 +5,29 @@ set -o pipefail
 set -u
 set -x
 
+compiler='gcc@7.2.0-spack'
+if [ $(uname) = Darwin ]; then
+    compiler1='clang@9.0.0'
+else
+    compiler1="$compiler"
+fi
+
 mv "../spack-tmux.old" "../spack-tmux" 2>/dev/null || true
 mv "../spack-tmux" "../spack-tmux.old" 2>/dev/null || true
 
-#TODO spack view -d false hardlink "../spack-tmux" lmod%gcc@7.2.0-spack
-spack view -d false hardlink -i "../spack-tmux" rsync%clang@9.0.0-apple
-spack view -d false hardlink -i "../spack-tmux" tmux%gcc@7.2.0-spack
+#TODO spack view -d false hardlink "../spack-tmux" lmod%"$compiler"
+spack view -d false hardlink -i "../spack-tmux" rsync%"$compiler1"
+spack view -d false hardlink -i "../spack-tmux" tmux%"$compiler"
 
 mv "../spack-view.old" "../spack-view" 2>/dev/null || true;
 mv "../spack-view" "../spack-view.old" 2>/dev/null || true;
 
-spack view -d true hardlink -i "../spack-view" cactusext%gcc@7.2.0-spack
+spack view -d true hardlink -i "../spack-view" cactusext%"$compiler"
 spack view -d true hardlink -i "../spack-view" gcc@7.2.0
 
 easy_install_file="../spack-view/lib/python2.7/site-packages/easy-install.pth"
 rm -f "$easy_install_file"
-for pkg in $(spack find -d -l cactusext%gcc@7.2.0-spack |
+for pkg in $(spack find -d -l cactusext%"$compiler" |
                  perl -p -e 's/\x1B\[[0-9;]*[a-zA-Z]//g' |
                  grep '\^py-' |
                  awk '{ print $1; }'); do
