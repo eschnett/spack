7d3d739a7f322f76c4672994a48cfd51047b7a0a
Erik Schnetter <schnetter@gmail.com>
2018-02-06 13:56:05 -0500

    qthreads: Update to version 1.13

--- var/spack/repos/builtin/packages/qthreads/package.py
+++ var/spack/repos/builtin/packages/qthreads/package.py
@@ -39,6 +39,8 @@ class Qthreads(AutotoolsPackage):
     homepage = "http://www.cs.sandia.gov/qthreads/"
 
     url = "https://github.com/Qthreads/qthreads/releases/download/1.10/qthread-1.10.tar.bz2"
+
+    version("1.13", "65a88fb9d159b56ccc2b3e3086ef914c")
     version("1.12", "c857d175f8135eaa669f3f8fa0fb0c09")
     version("1.11", "68b5f9a41cfd1a2ac112cc4db0612326")
     version("1.10", "d1cf3cf3f30586921359f7840171e551")
