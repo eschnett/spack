193269994079d1b35d76c3fcc0bf51aafaef0121
Erik Schnetter <schnetter@gmail.com>
2018-02-07 16:54:37 -0500

    openmpi: Add missing import statement

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -23,8 +23,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 
-import re
 import os
+import re
+import sys
 
 from spack import *
 
