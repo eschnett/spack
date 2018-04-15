2a4d41c66d07dc55f4e4a9069d623f07b53f56e2
Erik Schnetter <schnetter@gmail.com>
2018-04-15 16:37:58 -0400

    SimulationIO: New version 2.6.0

--- var/spack/repos/builtin/packages/simulationio/package.py
+++ var/spack/repos/builtin/packages/simulationio/package.py
@@ -31,6 +31,7 @@ class Simulationio(CMakePackage):
     homepage = "https://github.com/eschnett/SimulationIO"
     url      = "https://github.com/eschnett/SimulationIO/archive/version/0.1.0.tar.gz"
 
+    version('2.6.0', 'a819e1907b470d95fda0cfe9ca34a063')
     version('2.2.0', '2284d3908767986ef0c3c9d92397cebe')
     version('2.1.0', 'f8dab2873a4d277e9a655a58bb9f259c')
     version('2.0.0', 'e831b10641a6091c12cd0cb3bc2b228d')
