7a3ba00bc11a9c38446c4bc135a22f2eb26f2ec6
Erik Schnetter <schnetter@gmail.com>
2018-08-14 11:59:11 -0400

    Install desired rsync

--- bin/install.sh
+++ bin/install.sh
@@ -9,14 +9,14 @@ compiler='gcc@8.2.0-spack'
 if [ $(uname) = Darwin ]; then
     compiler1='clang@9.1.0'
 else
-    compiler1="$compiler"
+    compiler1="$compiler"       # this is wrong
 fi
 
 mv "../spack-tmux.old" "../spack-tmux" 2>/dev/null || true
 mv "../spack-tmux" "../spack-tmux.old" 2>/dev/null || true
 
 #TODO spack view -d false hardlink "../spack-tmux" lmod%"$compiler"
-spack view -d false hardlink -i "../spack-tmux" rsync
+spack view -d false hardlink -i "../spack-tmux" rsync%"$compiler"
 spack view -d false hardlink -i "../spack-tmux" tmux%"$compiler"
 
 mv "../spack-view.old" "../spack-view" 2>/dev/null || true;
